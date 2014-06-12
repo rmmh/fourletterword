f5587c11094ab9991a077a8e21194249ffd4e820
Ryan Hitchman <hitchmanr@gmail.com>
2014-06-11 23:56:47 -0700

    Makefile, general cleanups

--- generate.c
+++ generate.c
@@ -1,14 +1,16 @@
 #include <assert.h>
 #include <error.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 ////////////////////////////
 // xorshift128+ generator //
 ////////////////////////////
-uint64_t s[ 2 ];
+static uint64_t s[ 2 ];
 
-static uint64_t next(void) {
+static uint64_t xrand(void) {
     uint64_t s1 = s[ 0 ];
     const uint64_t s0 = s[ 1 ];
     s[ 0 ] = s0;
@@ -17,7 +19,7 @@ static uint64_t next(void) {
 }
 ////////////////////////////
 
-int int_compar(const void *a, const void *b)
+static int int_compar(const void *a, const void *b)
 {
     return *(int*)a - *(int*)b;
 }
@@ -26,15 +28,15 @@ int int_compar(const void *a, const void *b)
 
 char words[NUM_WORDS][5];
 
-uint8_t alphabet[26];
-uint8_t alphabet_rev[26];
+uint8_t letter_to_value[26];
+uint8_t value_to_letter[26];
 
 static int word_to_int(char *word)
 {
+    int i;
     int ret = 0;
-    do {
-        ret = (ret * 26) + alphabet[(*word - 'A')];
-    } while (*++word);
+    for (i = 3; i >= 0; i--)
+        ret = (ret * 26) + letter_to_value[word[i] - 'A'];
     return ret;
 }
 
@@ -42,55 +44,42 @@ static void build_rev(void)
 {
     int i;
     for (i = 0; i < 26; i++) {
-        alphabet_rev[alphabet[i]] = i;
+        value_to_letter[letter_to_value[i]] = i;
     }
-}
-
-static int word_to_int_le(char *word)
-{
-    int i;
-    int ret = 0;
-    for (i = 3; i >= 0; i--)
-        ret = (ret * 26) + alphabet[(word[i] - 'A')];
-    return ret;
 }
 
 static void int_to_word(int word_int, char *out)
 {
     build_rev();
     int i;
-    for (i = 3; i >= 0; i--) {
-        out[i] = alphabet_rev[word_int % 26] + 'A';
-        word_int /= 26;
-    }
-    out[4] = 0;
-}
-
-static void int_to_word_le(int word_int, char *out)
-{
-    build_rev();
-    int i;
     for (i = 0; i < 4; i++) {
-        out[i] = alphabet_rev[word_int % 26] + 'A';
+        out[i] = value_to_letter[word_int % 26] + 'A';
         word_int /= 26;
     }
     out[4] = 0;
 }
 
-static void shuffle_alphabet(void)
+// Initialize buf to a permutation -- each value in [0, len) is present
+static void gen_permutation(uint8_t *buf, int len)
 {
     int i, j;
-
-    // initialize & shuffle
-    for (i = 0; i < 26; i++) {
-        j = next() % (i + 1);
+    for (i = 0; i < len; i++) {
+        j = xrand() % (i + 1);
         if (j != i)
-            alphabet[i] = alphabet[j];
-        alphabet[j] = i;
+            buf[i] = buf[j];
+        buf[j] = i;
     }
 }
 
-static int bit_size(int delta)
+// compute size of value encoded with variable-nibble format
+//
+// Bit pattern            Values
+// 0xxx:                    0-7
+// 1xxx0xxx:                8-71
+// 1xxx1xxx0xxx:           72-583
+// 1xxx1xxx1xxx0xxx:      584-4679
+// 1xxx1xxx1xxx1xxx0xxx: 4680-37448
+static unsigned nibble_encoded_size(int delta)
 {
     if (delta < 0b1000)
         return 4;
@@ -104,76 +93,71 @@ static int bit_size(int delta)
         return 20;
     else if (delta < 0b1001001001001001000)
         return 24;
-    errx(1, "huge valued %d", delta);
+    return 10000;
 }
 
-static int compute_efficiency(void)
+// determine how many bits it would take to encode the words
+// with the current letter_to_value
+static unsigned compute_efficiency(void)
 {
+    /* convert words to ints */
     int i;
     int word_values[NUM_WORDS];
     for (i = 0; i < NUM_WORDS; i++)
-        word_values[i] = word_to_int_le(words[i]);
+        word_values[i] = word_to_int(words[i]);
     qsort(word_values, NUM_WORDS, sizeof(int), int_compar);
 
+    /* compress list of strictly increasing integers by
+     * encoding difference from previous word into nibbles*/
     int last = 0;
-    int bits = 0;
+    unsigned bits = 0;
     for (i = 0; i < NUM_WORDS; i++) {
         int delta = word_values[i] - last - 1;
         last = word_values[i];
-        bits += bit_size(delta);
-        continue;
-
-        char word[5];
-        int_to_word(word_values[i], word);
-        printf("%d %d %d %s\n", word_values[i], delta, bit_size(delta), word);
+        bits += nibble_encoded_size(delta);
     }
 
     return bits;
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
     int i;
     FILE *wordlist = fopen("list.txt", "r");
     for (i = 0; i < NUM_WORDS; i++)
-        fscanf(wordlist, " %4s ", &words[i]);
+        fscanf(wordlist, " %4s ", words[i]);
     fclose(wordlist);
 
     FILE *rand = fopen("/dev/urandom", "r");
     fread(&s, sizeof(uint64_t), 2, rand);
     fclose(rand);
 
-    shuffle_alphabet();
+    gen_permutation(letter_to_value, 26);
 
     for (i = 0; i < NUM_WORDS; i++) {
-        char *word = words[i];
         char word_again[5];
-        int word_val = word_to_int_le(words[i]);
-        int_to_word_le(word_val, word_again);
+        int word_val = word_to_int(words[i]);
+        int_to_word(word_val, word_again);
         assert(!strcmp(words[i], word_again));
-        //printf("%s %d %s\n", word, word_val, word_again);
     }
 
-    int best = 10000;
+    unsigned best = -1;
     long long iter = 0;
     while (1) {
-        shuffle_alphabet();
-        int bits = compute_efficiency();
-        int bytes = (bits + 7) / 8 + 26;
+        gen_permutation(letter_to_value, 26);
+        unsigned bits = compute_efficiency();
+        unsigned bytes = (bits + 7) / 8 + 26;
         iter++;
         if (!(iter & 0xFFFFF)) {
-            printf("iters:%ld\n", iter);
+            printf("iters: %lld\n", iter);
         }
         if (bytes <= best) {
-            best = bytes;
-            printf("%4d alphabet: ", bytes);
-
             build_rev();
+            for (i = 0; i < 26; i++)
+                printf("%c", 'a' + value_to_letter[i]);
+            printf("  bytes: %d  iters: %lld\n", bytes, iter);
 
-            for (i = 0; i < 26; i++) printf("%c", 'a' + alphabet[i]);
-            printf("   rev: ");
-            for (i = 0; i < 26; i++) printf("%c", 'a' + alphabet_rev[i]);
-            printf(" iters:%ld\n", iter);
+            best = bytes;
             iter = 0;
         }
     }
